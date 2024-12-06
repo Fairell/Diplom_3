d80e776fa6269c6f29470fa2109595059f087f03
Fairell <fairell@vk.com>
2024-12-06 23:47:25 +0300

    added Globals class for url addresses

---
+++ src/main/java/pageobjects/Globals.java
@@ -0,0 +1,7 @@
+package pageobjects;
+
+public class Globals {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    public static final String REGISTER_PAGE = "https://stellarburgers.nomoreparties.site/register";
+    public static final String FORGOT_PASSWORD_PAGE = "https://stellarburgers.nomoreparties.site/forgot-password";
+}
